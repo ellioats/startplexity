d8c90594997267362bc994fa3984fe88539eeb9c
Elliot <emwcurrie@gmail.com>
2025-05-21 22:13:27 -0400

    created object to represent data sent to api.

--- lib/sonarUtils.ts
+++ lib/sonarUtils.ts
@@ -1,5 +1,23 @@
 // initial query to be sent to perplexity 
-export const startQuery = "";
+
+import { questionOne, questionTwo, questionThree } from "./questions";
+
+export const startQuery = "
+    A client
+";
+
+export default function formatQuery(
+  initialIdea: string,
+  answer1: string,
+  answer2: string,
+  answer3: string
+) {
+
+
+
+}
+
+
 
 
 
