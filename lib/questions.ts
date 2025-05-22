3e72f1b12b69d340177d8a86c27a1712499f458e
Elliot <emwcurrie@gmail.com>
2025-05-21 22:12:37 -0400

    added third question

--- lib/questions.ts
+++ lib/questions.ts
@@ -1,4 +1,4 @@
 export const questionOne = "Who is your ideal user or customer, and what specific problem are you solving for them?";
 export const questionTwo = " What makes your solution different from — or better than — existing alternatives?";
-export const questionThree = "";
+export const questionThree = "What would be the smallest version of this idea you could launch to test demand?";
 
