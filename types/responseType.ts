8076a35766b2c3b453acdb56d3e698845b77b112
Elliot <emwcurrie@gmail.com>
2025-05-21 22:14:34 -0400

    added query data object, to be json.stringified later

---
+++ types/responseType.ts
@@ -0,0 +1,12 @@
+export interface responseType {
+
+  initialQuery: string,
+
+  questionOne: string,
+  questionTwo: string,
+  questionThree: string,
+
+  responseOne: string,
+  responseTwo: string,
+  responseThree: string
+}
