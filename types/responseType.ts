650f0a9f4df27bfc56832073f8fa807080e3ac0e
Elliot <emwcurrie@gmail.com>
2025-05-22 22:57:49 -0400

    added documentation

--- types/responseType.ts
+++ types/responseType.ts
@@ -1,4 +1,4 @@
-// do not use. still wip
+// use for the api request, fill in data from context user input + static queries specified in lib/questions
 export interface responseType {
 
   initialQuery: string,
