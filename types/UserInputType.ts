9e0ea91d8a08951d089a73196308da0c0faa8674
Elliot <emwcurrie@gmail.com>
2025-05-22 20:54:28 -0400

    moved user input typedef into another file

---
+++ types/UserInputType.ts
@@ -0,0 +1,7 @@
+// used for context that stores user data, avoiding prop drilling
+export type UserInput = {
+  initialQuery: string;
+  answerOne: string;
+  answerTwo: string;
+  answerThree: string;
+}
