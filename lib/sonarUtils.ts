145fd17c10134490c46b8d58116b0b9b8c7d2f60
Elliot <emwcurrie@gmail.com>
2025-05-24 00:02:26 -0400

    added more prompts

--- lib/sonarUtils.ts
+++ lib/sonarUtils.ts
@@ -1,11 +1,12 @@
 
-// fill these out 
+// fill these out, convert to enum later?
 const defaultPrompt = "You are a startup strategist, product analyst, and technical writer. The input will consist of four things. Their overall idea, and the answers to three questions. 1: Who is your ideal user or customer, and what specific problem are you solving for them? 2: What makes your solution different from — or better than — existing alternatives? 3: What would be the smallest version of this idea you could launch to test demand? Your task is to provide the plaintext of two documents, seperated three returns an 5 equal signs. These two documents are the employement contract and the business plan. Make these documents according to the user's input.";
+
 const employementContractAddition = "Your task is to provide the plaintext of the employment contract for the provided startup. If your output contains other text, please seperate the employement contract text with '=' characters.";
 const businessPlanAddition = "Your task is to provide the plaintext of the business plan document for the provided startup. If your output contains other text, please seperate the business plan text with '=' characters";
 const employmentAndBusinessAddition = "Your task is to provide the plaintext of two documents, seperated three returns an 5 equal signs. These two documents are the employement contract and the business plan. Make these documents according to the user's input.";
-
-
-
+const shareHoldersAgreementAddition = "Your task is to provide the plaintext of the shareholders agreement document for the provided startup. If your output contains other text, please seperate the business plan text with '=' characters";
+const pitchDeckAddition = "Your task is to provide the plaintext of the pitch deck document for fundraising campaigns for the provided startup. If your output contains other text, please seperate the business plan text with '=' characters ";
+const termsOfServiceAddition = "Your task is to provide the plaintext of the terms of service agreement for the provided startup. If your output contains other text, please seperate the business plan text with '=' characters"
 
 
